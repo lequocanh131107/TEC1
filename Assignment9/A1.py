dc09717c8922886988be0fca32e6babb8ca704e0
qanh <you@example.com>
2026-04-03 23:50:10 +0700

    finish A1

--- Assignment9/A1.py
+++ Assignment9/A1.py
@@ -1,1 +1,12 @@
-print ( "hello world" )
+
+count = 0
+print("Enter your text ( Ấn enter để kích hoạt lệnh break):")
+
+while True:
+    line = input()
+    if line == "": 
+        break
+    if line.strip() != "":
+        count += 1
+
+print(f"tổng số dòng là: {count}")
