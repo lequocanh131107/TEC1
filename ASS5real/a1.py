aa54c9d2946f332172d091842d30d3f8b55a5870
lequocanh131107 <lequocanh131107@gmail.com>
2026-04-10 00:38:35 +0700

    Delete ASS5real directory

--- ASS5real/a1.py
+++
@@ -1,8 +0,0 @@
-numbers = []
-while True:
-    x = input(" nhap so ( cach de huy):")
-    if x == "":
-       break
-    numbers.append(int(x))
-numbers.sort(reverse=True)
-print (" 5 so lon nhat la:",numbers[:5])
